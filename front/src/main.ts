c1c963148a8935c0865e41fa6f3e3a1754bc28cf
我的六月天 <1358558863@qq.com>
2024-01-22 13:41:19 +0800

    引入antd

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from "./router";
+// ant-design-vue
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App);
 
