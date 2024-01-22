46085395389c082d9aea5f494b804028c51e1911
我的六月天 <1358558863@qq.com>
2024-01-22 12:20:34 +0800

    初始化server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+
+const express = require('express')   //引入express
+const bodyParser = require('body-parser')   // 解析参数
+
+const app = express();
+
+app.use(bodyParser())  // 获取 post请求传递来的参数
+const port = 6000  //（监听的端口号）
+
+// 解决跨域
+app.all("*", function (req, res, next) {
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers", "content-type");
+    //跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    next();
+});
+ 
+// 测试是否连接成功
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send({
+        data: '连接成功',
+        status: 200
+    })
+})
+ 
+app.listen(port, () => {
+    console.log(`连接成功`)
+})
