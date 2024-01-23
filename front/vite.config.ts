c28e3c6243b3042c3630ce54f35ff0e569fd3a4e
我的六月天 <1358558863@qq.com>
2024-01-23 14:17:37 +0800

    设置代理，解决跨域

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -15,4 +15,20 @@ export default defineConfig({
       ],
     }),
   ],
+
+  //服务器配置
+  server: {
+    open: "/", // 浏览器自动打开 关闭false
+    strictPort: true, //端口占用，自动尝试下一个端口
+    //代理配置
+    proxy: {
+      "/demo": {
+        target: "http://121.40.108.95:8080", // api接口代理,
+        changeOrigin: true,
+      },
+    },
+    hmr: {
+      overlay: false, //报错不显示在页面上
+    },
+  },
 })
