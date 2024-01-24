4e062d9dc36990f075e180c283b2356958606416
我的六月天 <1358558863@qq.com>
2024-01-24 17:10:43 +0800

    引入图标和拖动库

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,8 +4,13 @@ import App from './App.vue';
 import router from "./router";
 // ant-design-vue
 import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 
 const app = createApp(App);
+for (const i in Icons) {
+    // 全局注册一下组件
+    app.component(i, Icons[i])
+}
 
 app.use(router);
 
