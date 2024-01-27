b067839e75ee90e147523b96e814f4134be07b1a
我的六月天 <1358558863@qq.com>
2024-01-27 08:56:07 +0800

    增加stationList

--- front/src/pages/view/Home.vue
+++ front/src/pages/view/Home.vue
@@ -9,14 +9,7 @@
         <a-col>
             <span style="font-size: 1.2rem;font-weight: 600;">站点：</span>
             <a-radio-group class="menu" v-model:value="site" @change="handleChange" button-style="solid" size="large">
-                <a-radio-button value="1">线路所</a-radio-button>
-                <a-radio-button value="2">阜阳西</a-radio-button>
-                <a-radio-button value="3">太和东</a-radio-button>
-                <a-radio-button value="4">临泉</a-radio-button>
-                <a-radio-button value="5">界首</a-radio-button>
-                <a-radio-button value="6">古城东</a-radio-button>
-                <a-radio-button value="7">毫州南</a-radio-button>
-                <a-radio-button value="8">芦庙</a-radio-button>
+                <a-radio-button v-for="item in stationList" :value="item.stationId">{{ item.name }}</a-radio-button>
             </a-radio-group>
         </a-col>
     </a-row>
@@ -24,11 +17,14 @@
 </template>
 
 <script lang="ts">
+import Utils from "../../util/utils";
+
 
 export default {
     data() {
         return {
             site: "0",
+            stationList: Utils.stationList,
         }
     },
     async created() {
--- front/src/util/utils.ts
+++ front/src/util/utils.ts
@@ -1,8 +1,19 @@
-const Utils =  {
+const Utils = {
     /**下载文件接口 */
     filePrefix: '/demo/file/download.json?id=',
     /**上传文件接口 */
-    uploadUrl : '/demo/file/upload.json',
+    uploadUrl: '/demo/file/upload.json',
+    /**站点列表 */
+    stationList: [
+        { stationId: '1', name: "线路所" },
+        { stationId: '2', name: "阜阳西" },
+        { stationId: '3', name: "太和东" },
+        { stationId: '4', name: "临泉" },
+        { stationId: '5', name: "界首" },
+        { stationId: '6', name: "古城东" },
+        { stationId: '7', name: "毫州南" },
+        { stationId: '8', name: "芦庙" },
+    ],
 }
 
-export default  Utils;
+export default Utils;
