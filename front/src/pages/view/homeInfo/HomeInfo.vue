eeb1e906410694cf1e4f78a1aa52f5684e864ab4
我的六月天 <1358558863@qq.com>
2024-01-27 10:01:07 +0800

    增加站点前缀

--- front/src/pages/view/homeInfo/HomeInfo.vue
+++ front/src/pages/view/homeInfo/HomeInfo.vue
@@ -23,6 +23,7 @@
 
 <script lang="ts">
 import http from "../../../util/http";
+import Utils from "../../../util/Utils";
 import moment from 'moment';
 
 interface DataItem {
@@ -61,13 +62,22 @@ export default {
         async getRemainInfo() {
             const res0 = await http.get("/demo/device/checkCycleReminder.json");
             if (res0 && res0.data && res0.data.status === 200) {
-                this.data[0].list = res0.data.data.map(info => `${info.name}应在${moment(info.nextFixTime).format("YYYY-MM-DD hh:mm:ss")}检查`);
+                this.data[0].list = res0.data.data.map(info => `[${this.getStationName(info.stationId)}] ${info.name}应在${moment(info.nextFixTime).format("YYYY-MM-DD hh:mm:ss")}检查`);
             }
             const res1 = await http.get("/demo/device/recentMaintenanceReminder.json");
             if (res1 && res1.data && res1.data.status === 200) {
-                this.data[1].list = res0.data.data.map(info => `${info.name}应在${moment(info.nextFixTime).format("YYYY-MM-DD hh:mm:ss")}养修`);
+                this.data[1].list = res0.data.data.map(info => `[${this.getStationName(info.stationId)}] ${info.name}应在${moment(info.nextFixTime).format("YYYY-MM-DD hh:mm:ss")}养修`);
             }
-        }
+        },
+        getStationName(stationId){
+            let statioName = Utils.stationList[0].name;
+            Utils.stationList.forEach(item => {
+                if (+item.stationId === stationId) {
+                    statioName = item.name;
+                }
+            });
+            return statioName;
+        },
     }
 }
 
