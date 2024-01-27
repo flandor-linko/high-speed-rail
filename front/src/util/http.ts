151e64d5b73b8fb23af58180c34b9204933b1707
我的六月天 <1358558863@qq.com>
2024-01-27 08:56:49 +0800

    延长超时时间为2分钟

--- front/src/util/http.ts
+++ front/src/util/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.headers.post["Content-Type"] = "application/json; charset=utf-8";
 axios.defaults.withCredentials = true; //允许跨域携带cookie信息
-axios.defaults.timeout = 10000; //请求超时
+axios.defaults.timeout = 120000; //请求超时
 // 创建axios实例
 let http = axios.create({
   baseURL: "", // api的base_url   import.meta.env.VITE_APP_BASE_API,
