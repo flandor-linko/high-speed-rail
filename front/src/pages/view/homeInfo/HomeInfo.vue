4ef55373744d9456c3795f4f375e2c5ae21f22b1
我的六月天 <1358558863@qq.com>
2024-02-05 09:02:01 +0800

    检查、养修提醒增加设备类型提示

--- front/src/pages/view/homeInfo/HomeInfo.vue
+++ front/src/pages/view/homeInfo/HomeInfo.vue
@@ -4,7 +4,11 @@
             <a-card :title="data[0].title" :bodyStyle="{ height: '40rem', overflow: 'auto' }">
                 <a-list size="small" bordered :data-source="data[0].list">
                     <template #renderItem="{ item }">
-                        <a-list-item @click="clickEquip(item.id)"><a-tag :color="getStationColor(item.stationId)">{{ getStationName(item.stationId) }}</a-tag> <span style="text-decoration: underline;">{{ item.name }}</span> 应在 <a-tag color="blue">{{ formatTime(item.nextFixTime) }}</a-tag> 检查</a-list-item>
+                        <a-list-item @click="clickEquip(item.id)"><a-tag :color="getStationColor(item.stationId)">{{
+                            getStationName(item.stationId) }}</a-tag> <span style="text-decoration: underline;">{{
+        item.name }}</span>（ {{ getTypeName(item.type) }} ） 应在 <a-tag color="blue">{{
+        formatTime(item.nextFixTime) }}</a-tag>
+                            检查</a-list-item>
                     </template>
                 </a-list>
             </a-card>
@@ -13,7 +17,11 @@
             <a-card :title="data[1].title" :bodyStyle="{ height: '40rem', overflow: 'auto' }">
                 <a-list size="small" bordered :data-source="data[1].list">
                     <template #renderItem="{ item }">
-                        <a-list-item @click="clickEquip(item.id)"><a-tag :color="getStationColor(item.stationId)">{{ getStationName(item.stationId) }}</a-tag> <span style="text-decoration: underline;">{{ item.name }}</span> 已在 <a-tag color="blue">{{ formatTime(item.lastFixTime) }}</a-tag> 完成养修</a-list-item>
+                        <a-list-item @click="clickEquip(item.id)"><a-tag :color="getStationColor(item.stationId)">{{
+                            getStationName(item.stationId) }}</a-tag> <span style="text-decoration: underline;">{{
+        item.name }}</span>（ {{ getTypeName(item.type) }} ） 已在 <a-tag color="blue">{{
+        formatTime(item.lastFixTime) }}</a-tag>
+                            完成养修</a-list-item>
                     </template>
                 </a-list>
             </a-card>
@@ -45,11 +53,13 @@ export default {
                     list: [],
                 },
             ] as DataItem[],
+            equipTypeList: [],
             checkList: [],
             remainList: [],
         }
     },
     async created() {
+        await this.getTypeList();
         await this.getRemainInfo();
         this.timer = setInterval(() => { this.getRemainInfo(); }, 60 * 1000);
     },
@@ -59,6 +69,12 @@ export default {
         }
     },
     methods: {
+        async getTypeList() {
+            const res = await http.get("/demo/deviceType/list.json");
+            if (res && res.data && res.data.status === 200) {
+                this.equipTypeList = res.data.data;
+            };
+        },
         async getRemainInfo() {
             const res0 = await http.get("/demo/device/checkCycleReminder.json");
             if (res0 && res0.data && res0.data.status === 200) {
@@ -78,7 +94,18 @@ export default {
             });
             return statioName;
         },
-        getStationColor(stationId){
+        getTypeName(typeId) {
+            let typeName = "";
+            for (let index = 0; index < this.equipTypeList.length; index++) {
+                const element = this.equipTypeList[index];
+                if (element.id === typeId) {
+                    typeName = element.type;
+                    break;
+                }
+            }
+            return typeName;
+        },
+        getStationColor(stationId) {
             let color = Utils.stationList[0].color;
             // Utils.stationList.forEach(item => {
             //     if (+item.stationId === stationId) {
@@ -87,7 +114,7 @@ export default {
             // });
             return color;
         },
-        formatTime(timeStr:string) {
+        formatTime(timeStr: string) {
             return moment(timeStr).zone("+08:00").format("YYYY-MM-DD");
         },
         clickEquip(id) {
@@ -102,5 +129,4 @@ export default {
 <style scoped>
 .title {
     font-weight: 800;
-}
-</style>
+}</style>
