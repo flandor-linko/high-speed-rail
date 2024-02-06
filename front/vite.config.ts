eebf7ba547125beeab6aa765c6e4920b76d6810f
我的六月天 <1358558863@qq.com>
2024-02-06 13:55:18 +0800

    解决 top level await编译报错

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,12 @@ export default defineConfig({
         }),
       ],
     }),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    })
   ],
 
   //服务器配置
